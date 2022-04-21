0e70bc6eba46765d873d5195b539a988eed88832
chrisrca <goalie.cyclone@gmail.com>
2022-04-20 20:26:11 -0400

    working compiled exe

---
+++ README.md
@@ -0,0 +1,2 @@
+When Building with OpenCV:
+pyinstaller --onedir --paths="C:\Users\goali\AppData\Local\Programs\Python\Python310\lib\site-packages\cv2" main.py --windowed
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ SCALEFACTOR = 1.22
 MINNEIGHBORS = 8       
 MINSIZE = (60, 60)    
 
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-cap = cv2.VideoCapture(0)
+faceCascade = cv2.CascadeClassifier("C:\\Users\\goali\\Projects\\Face Tracking\\haarcascade_frontalface_default.xml")
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 box = BoundingBox(-1, -1, -1, -1)
 
 while True:
